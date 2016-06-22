da459f39df3f9a7cf181f615219124eb571ea0c1
sh601857 <yijiong.hu@gmail.com>
2016-06-22 17:39:00 +0800

    test action

--- ANN/Test.py
+++ ANN/Test.py
@@ -6,7 +6,8 @@ import numpy
 #Download data from yahoo finance
 start = datetime.datetime(2005,1,1)
 end = datetime.datetime(2016,6,21)
-ticker = "000300" #^HSI  ^HSCE  
-f=web.DataReader(ticker,'google',start,end)
-print(f[:5])
-print(f.tail(5))
+ticker = "2318.HK" #^HSI  ^HSCE  
+#f=web.DataReader(ticker,'yahoo-actions',start,end)
+#f=web.DataReader(ticker,'yahoo',start,end)
+print(f)
+#print(f.tail(5))
