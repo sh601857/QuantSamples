8c3ffdaec7273a6f9b624a0cf6fd3c8f47809435
sh601857 <yijiong.hu@gmail.com>
2016-11-28 22:44:48 +0800

    u

--- HK/UqerDownload.py
+++ HK/UqerDownload.py
@@ -5,7 +5,8 @@ uqer = Client(username='huyijiong@139.com', password='qwe123!@#')
 #files = uqer.list_data()
 #print(files)
 
-tickers = ['00939','00966','00998','01177','01288','01339','01398','01918','01988','02318','02328','03618','03968','06818']
+tickers = ['00811','00939','00966','00998','01177','01288','01339','01398','01918','01988',
+           '02318','02328','02883','03288','03618','03899','03968','03988','06818']
 #for ticker in tickers:
 #    uqer.download_data( filename='HKQuotes/HK{0}.txt'.format(ticker) )
 
