cd936ce50287ff8a0517189356d647df46fb6e4a
sh601857 <yijiong.hu@gmail.com>
2017-03-26 22:51:42 +0800

    u

--- PortfolioMan/FundPerf.py
+++ PortfolioMan/FundPerf.py
@@ -8,12 +8,12 @@ wb = xw.Book('Funds.xlsx')
 sht = wb.sheets['FundPerf']
 
 for i in range(2,1000):    
-    code=sht.range('A{0}'.format(i)).value
+    code=sht.range('B{0}'.format(i)).value
     if code != None:
         npNav = SinaQuote.GetHNav( code[2:] )
         if len(npNav) > 1:
-            sht.range((i,3)).value = npNav.fbrq[-1]
-            sht.range((i,4)).value = npNav.adjNav[-1] / npNav.adjNav[-2] - 1.0
+            sht.range((i,4)).value = npNav.fbrq[-1]
+            sht.range((i,5)).value = npNav.adjNav[-1] / npNav.adjNav[-2] - 1.0
         for j in range(6,20):
             bdate = sht.range((1,j)).value
             if bdate != None:
@@ -25,6 +25,6 @@ for i in range(2,1000):
                 break
     else:
         break    
-wb.save()     
+#wb.save()     
 #wb.close()  
 
