9acc484adc7f99bc3cb0c183c5065fdc9cb04508
sh601857 <yijiong.hu@gmail.com>
2017-07-30 22:19:13 +0800

    U

--- PortfolioMan/TradeRec.py
+++ PortfolioMan/TradeRec.py
@@ -11,10 +11,10 @@ import xlwings as xw
 account = {'A764440052':'HHXHT','0162754560':'HHXHT', 'A760709852':'SFFHT','0162391596':'SFFHT','A103762289':'HYJHT','0124806514':'HYJHT',
 'A733917349':'HTSWP','0221666359':'HTSWP','A733919236':'JAXHT','0221666518':'JAXHT','C115038475':'HYJHB'}
 
-rec = pd.read_csv(r'C:\Users\Administrator\Documents\table.xls',sep='\t',header=0, index_col=None, dtype={u'股东帐户':str,u'证券代码':str}, encoding='gbk',
+rec = pd.read_csv(r'C:\Users\SFF\Documents\table.xls',sep='\t',header=0, index_col=None, dtype={u'股东帐户':str,u'证券代码':str}, encoding='gbk',
       usecols=[u'成交日期',u'证券代码',u'证券名称',u'摘要',u'合同编号',u'成交均价',u'成交数量',u'成交金额',u'发生金额',u'股东帐户'])
 rec = rec[ [u'成交日期',u'证券代码',u'证券名称',u'摘要',u'合同编号',u'成交均价',u'成交数量',u'成交金额',u'发生金额',u'股东帐户'] ]
 rec[u'合同编号'] = rec.index + 1
 rec[u'股东帐户'] = rec[u'股东帐户'].map( lambda x: account[x] )
-rec.to_csv(r'C:\Users\Administrator\Documents\table.xls', sep=',', index=False, encoding='gbk')
+rec.to_csv(r'C:\Users\SFF\Documents\table.xls', sep=',', index=False, encoding='gbk')
 print(rec)
