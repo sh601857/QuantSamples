ecd0cfe31896ef5021bb68a6264e682a007a8442
sh601857 <yijiong.hu@gmail.com>
2018-03-25 22:49:20 +0800

    U

--- PortfolioMan/TradeRecHTo.py
+++ PortfolioMan/TradeRecHTo.py
@@ -8,14 +8,15 @@ import xlwings as xw
 # sht = wb.sheets['table']
 # print( sht.range("D2").value )
 
-fpath = 'C:\\Users\\Administrator\\Documents\\'
+fpath = 'C:\\Users\\SFF\\Documents\\'
 
-account = {'A242096229':'LLFHTo','0243781523':'LLFHTo',}
+account = {'A242096229':'LLFHTo','0243781523':'LLFHTo','0124806514':'HYJHTo',}
 
 rec = pd.read_csv(fpath+'table.xls',sep='\t',header=0, index_col=None, dtype={u'股东帐户':str,u'证券代码':str}, encoding='gbk',
       usecols=[u'成交日期',u'证券代码',u'证券名称',u'操作',u'合同编号',u'成交均价',u'成交数量',u'成交金额',u'发生金额',u'股东帐户'])
 rec = rec[ [u'成交日期',u'证券代码',u'证券名称',u'操作',u'合同编号',u'成交均价',u'成交数量',u'成交金额',u'发生金额',u'股东帐户'] ]
-rec[u'合同编号'] = rec.index + 1
+
 rec[u'股东帐户'] = rec[u'股东帐户'].map( lambda x: account[x] )
+rec[u'合同编号'] = rec.index + 60001
 rec.to_csv(fpath+'table.xls', sep=',', index=False, encoding='gbk')
 print(rec)
