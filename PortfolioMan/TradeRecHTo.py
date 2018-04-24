7796d4db928a5a92372cffd4088dcaccf3dd8526
sh601857 <yijiong.hu@gmail.com>
2018-04-24 17:39:25 +0800

    'uuu'

--- PortfolioMan/TradeRecHTo.py
+++ PortfolioMan/TradeRecHTo.py
@@ -16,6 +16,10 @@ rec = pd.read_csv(fpath+'table.xls',sep='\t',header=0, index_col=None, dtype={u'
       usecols=[u'成交日期',u'证券代码',u'证券名称',u'操作',u'合同编号',u'成交均价',u'成交数量',u'成交金额',u'发生金额',u'股东帐户'])
 rec = rec[ [u'成交日期',u'证券代码',u'证券名称',u'操作',u'合同编号',u'成交均价',u'成交数量',u'成交金额',u'发生金额',u'股东帐户'] ]
 
+for i in range( len(rec) ):
+    if u'卖出' in rec.iloc[i,3]:
+        rec.iloc[i,6] = - rec.iloc[i,6] 
+		
 rec[u'股东帐户'] = rec[u'股东帐户'].map( lambda x: account[x] )
 rec[u'合同编号'] = rec.index + 60001
 rec.to_csv(fpath+'table.xls', sep=',', index=False, encoding='gbk')
