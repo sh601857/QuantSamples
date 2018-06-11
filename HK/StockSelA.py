3d234270d228b89e6467462beccc6586b234ecfd
sh601857 <yijiong.hu@gmail.com>
2018-06-11 18:19:01 +0800

    'uuu'

--- HK/StockSelA.py
+++ HK/StockSelA.py
@@ -3,9 +3,9 @@ import pandas as pd
 import numpy as np
 import xlwings as xw
 
-wb = xw.Book(u'D:\\yun\\百度云\\FinExcels\\uq\\FRA2_DivRatio_UQ.xlsx')
-#wb = xw.Book(u'D:\\yun\\百度云\\FinExcels\\reports\\StocksSelA_14_16_5.xlsx')
-templ = wb.sheets['000022']
+wb = xw.Book(u'D:\\yun\\百度云\\FinExcels\\StockSel\\StocksSelRoe15_y16_13_16_D.xlsx')
+#wb = xw.Book(u'D:\\yun\\百度云\\FinExcels\\reports\\StocksSelA_14_16_5_D.xlsx')
+templ = wb.sheets['002749']
 sht = wb.sheets['I']
 row =2
 for s in wb.sheets:
@@ -16,11 +16,11 @@ for s in wb.sheets:
         s.range('A1').value =  '=HYPERLINK(\"#\'{0}\'!C{2}\",\"{1}\")'.format(sht.name, 'Index', '{0}'.format(row) )
         s.range('A1').column_width = 20
         
-        templ.range('A2:A200').api.Copy()
-        s.range('A2:A200').api.PasteSpecial( -4104 )
+        templ.range('A2:A146').api.Copy()
+        s.range('A2:A146').api.PasteSpecial( -4104 )
         
-        templ.range('A1:Z200').api.Copy()
-        s.range('A1:Z200').api.PasteSpecial( -4122 )
+        templ.range('A1:Z146').api.Copy()
+        s.range('A1:Z146').api.PasteSpecial( -4122 )
         #s.range('B1:Z200').columns.autofit()
         s.range('B1:Z200').column_width = 7
         row=row+1
